89a68fb2d0b7292dbd51c58f998a9dff5004e1e2
Marx JMoura <marx.jmoura@gmail.com>
2019-09-21 10:05:46 -0300

    Remove unnecessary method from ImageFormData class

--- src/Storefront.Gallery.API/Models/TransferModel/ImageFormData.cs
+++ src/Storefront.Gallery.API/Models/TransferModel/ImageFormData.cs
@@ -1,6 +1,4 @@
 using System.ComponentModel.DataAnnotations;
-using System.IO;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Storefront.Gallery.API.Swagger;
 
@@ -24,15 +22,5 @@ namespace Storefront.Gallery.API.Models.TransferModel
         [SwaggerExclude]
         [Range(0, Max5MB, ErrorMessage = "File must be an image with a maximum size of 5MB.")]
         public long Size => File?.Length ?? 0;
-
-        public async Task<byte[]> ToByteArray()
-        {
-            using (var stream = new MemoryStream())
-            {
-                await File.CopyToAsync(stream);
-
-                return stream.ToArray();
-            }
-        }
     }
 }
